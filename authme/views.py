945c1ce69f8f33ef6af4ec7a524701d7e7762177
Durgesh <durgeksh@gmail.com>
2022-07-28 11:42:49 +0530

    1. Added urls.py in authme app 2. Written class Welcome in views.py to display default page 3. Chaged config/urls.py to set default view to Welcome class in urlpatterns list

--- authme/views.py
+++ authme/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+class Welcome(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        return Response("<h1>Welcome to the my web application!")
